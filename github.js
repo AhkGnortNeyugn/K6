72d4e914133679ab130ceb551500e52324b11ddb
Nguyễn Trọng Kha <32507584+AhkGnortNeyugn@users.noreply.github.com>
2023-06-25 18:29:17 +0700

    Test Github Action

---
+++ github.js
@@ -0,0 +1,16 @@
+import http from 'k6/http';
+import { sleep } from 'k6';
+
+export const options = {
+    vus: 5,
+    duration: '1m',
+    thresholds: {
+        http_req_failed: ['rate<0.01'],
+        http_req_duration: ['p(95)<300'],
+      },
+  };
+
+export default function () {
+  http.get('https://test.k6.io');
+  sleep(1);
+}
