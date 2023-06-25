b307be21e0b9ecfdd3cbbad0497553bece48bd1e
Nguyễn Trọng Kha <32507584+AhkGnortNeyugn@users.noreply.github.com>
2023-06-25 18:57:36 +0700

    Update vus

--- github.js
+++ github.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    vus: 5,
+    vus: 10,
     duration: '1m',
     thresholds: {
         http_req_failed: ['rate<0.01'],
